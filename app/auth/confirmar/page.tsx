4209041e9cef66f814edde02c400ba1a1fb5a0f5
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-23 18:22:20 -0500

    Inicio: Paginas de confirmar y reestablecer

---
+++ app/auth/confirmar/page.tsx
@@ -0,0 +1,3 @@
+export default function ConfirmarPage() {
+  return <div>ConfirmarPage</div>;
+}
---
+++ app/auth/reestablecer/page.tsx
@@ -0,0 +1,3 @@
+export default function ReestablecerPage() {
+  return <div>ReestablecerPage</div>;
+}
