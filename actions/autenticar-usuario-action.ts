79a9401527e6f7894b915b74c7753ae170c10a47
Ariel Hurtado <arielhurtado388@gmail.com>
2025-12-28 16:38:21 -0500

    MOstrar errores del server y colocar jwt en cookies

--- actions/autenticar-usuario-action.ts
+++ actions/autenticar-usuario-action.ts
@@ -1,6 +1,7 @@
 "use server";
 
-import { LoginSchema } from "@/src/schemas";
+import { cookies } from "next/headers";
+import { ErrorResponseSchema, LoginSchema } from "@/src/schemas";
 
 type ActionStateType = {
   errores: string[];
@@ -21,6 +22,36 @@ export async function autneticar(
       errores: auth.error.issues.map((issue) => issue.message),
     };
   }
+
+  const url = `${process.env.API_URL}/auth/iniciar-sesion`;
+  const req = await fetch(url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      correo: auth.data.correo,
+      password: auth.data.password,
+    }),
+  });
+
+  const json = await req.json();
+
+  if (!req.ok) {
+    const { error } = ErrorResponseSchema.parse(json);
+    return {
+      errores: [error],
+    };
+  }
+
+  // Setear cookies
+  cookies().set({
+    name: "LUKIN_TOKEN",
+    value: json,
+    httpOnly: true,
+    path: "/",
+  });
+
   return {
     errores: [],
   };
